3e21a46a58f6ee13b7841f3a2071abf8bfee2572
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2024-01-16 15:48:51 +0200

    fixing bugs

--- src/components/Blogs/BlogsData.tsx
+++ src/components/Blogs/BlogsData.tsx
@@ -1,10 +1,10 @@
 import { Blog } from "@/utils/types";
-import blogImg1 from "@components/blogs/imgs/blog-01.webp";
-import blogImg2 from "@components/blogs/imgs/blog-02.webp";
-import blogImg3 from "@components/blogs/imgs/blog-03.webp";
-import authorImg1 from "@components/blogs/imgs/auth-01.webp";
-import authorImg2 from "@components/blogs/imgs/auth-02.webp";
-import authorImg3 from "@components/blogs/imgs/auth-03.webp";
+import blogImg1 from "@components/Blogs/imgs/blog-01.webp";
+import blogImg2 from "@components/Blogs/imgs/blog-02.webp";
+import blogImg3 from "@components/Blogs/imgs/blog-03.webp";
+import authorImg1 from "@components/Blogs/imgs/auth-01.webp";
+import authorImg2 from "@components/Blogs/imgs/auth-02.webp";
+import authorImg3 from "@components/Blogs/imgs/auth-03.webp";
 
 export const BLOGS: Blog[] = [
   {
